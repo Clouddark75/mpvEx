bba2d092ef40a30d5ec4d7f24e2baaa6cdf357f1
Clouddark75 <84534855+Clouddark75@users.noreply.github.com>
2026-03-21 11:06:19 -0400

    Update AdvancedPreferences.kt

--- app/src/main/java/app/marlboroadvance/mpvex/preferences/AdvancedPreferences.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/preferences/AdvancedPreferences.kt
@@ -6,14 +6,11 @@ import app.marlboroadvance.mpvex.preferences.preference.PreferenceStore
 class AdvancedPreferences(
   preferenceStore: PreferenceStore,
 ) {
-  val mpvConfStorageUri = preferenceStore.getString("mpv_conf_storage_location_uri")
+  // Cambiado de URI a path directo
+  val mpvConfStorageLocation = preferenceStore.getString("mpv_conf_storage_location")
   val mpvConf = preferenceStore.getString("mpv.conf")
   val inputConf = preferenceStore.getString("input.conf")
-
   val verboseLogging = preferenceStore.getBoolean("verbose_logging", BuildConfig.BUILD_TYPE != "release")
-
   val enabledStatisticsPage = preferenceStore.getInt("enabled_stats_page", 0)
-
   val enableRecentlyPlayed = preferenceStore.getBoolean("enable_recently_played", true)
-
 }
